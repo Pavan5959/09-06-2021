a85299792ad6ad0b29ce873baeb4eb9d8f180681
PAVAN R <82048445+Pavan5959@users.noreply.github.com>
2021-06-09 20:40:33 +0530

    Create variable.js

---
+++ component/variable.js
@@ -0,0 +1,20 @@
+export let keyValue = 1000;
+
+export function test()
+{
+    keyValue = 2000;
+    console.log('tested');
+}
+
+
+/* To export in other way */
+
+/*let keyValue = 1000;
+
+function test()
+{
+    keyValue = 2000;
+    console.log('tested');
+}
+
+export {keyValue, test};*/
